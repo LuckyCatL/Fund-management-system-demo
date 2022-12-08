4b6e099adbb4dde8a4109572f19589c8791f52a1
LuckyCat <101119199+LuckyCatL@users.noreply.github.com>
2022-12-08 12:21:04 +0800

    Update main.js

--- client(前端)/src/main.js
+++ client(前端)/src/main.js
@@ -6,10 +6,11 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import axios from './http'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 
 const app = createApp(App)
-app.use(store).use(router).use(ElementPlus)
+app.use(store).use(router).use(ElementPlus, {locale: zhCn})
 app.config.globalProperties.$axios = axios;
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
